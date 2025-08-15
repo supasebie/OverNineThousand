4dfb3cb40999371462ef1eafd61bbd310a8c03be
Sebastian <supasebie@gmail.com>
2025-08-14 17:18:06 -0700

    chore: fix ESLint config and remove remaining explicit any usages; build passes locally

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,8 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// CommonJS version to avoid ESM loader issues in CI/Vercel
+const { FlatCompat } = require("@eslint/eslintrc");
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals", "next/typescript")];
-
-export default eslintConfig;
+module.exports = [...compat.extends("next/core-web-vitals", "next/typescript")];
